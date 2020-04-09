bdab2fc664680532d08d0f6dba0c08ed42c0fddc
peyman iravani <peyman.iravani.altranitalia@windtre.it>
2020-04-09 21:24:05 +0200

    update - last touch

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -12,71 +12,73 @@ export default class Projects extends Component {
                 <h2 className="colorlib-heading animate-box">Recent Projects</h2>
               </div>
             </div>
-            <div className="row">
-              <div className="col-md-4 animate-box" data-animate-effect="fadeInLeft">
-                <div className="project" style={{ backgroundImage: 'url(images/Herditama.jpg)' }}>
-                  <div className="desc">
-                    <div className="con">
-                      <h3><a href="https://herditama.netlify.com/">Hotel reservation</a></h3>
-                      <span>Hotel reservation App designed with reactjs</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4 animate-box" data-animate-effect="fadeInRight">
-                <div className="project" style={{ backgroundImage: 'url(images/dispache.png)' }}>
-                  <div className="desc">
-                    <div className="con">
-                      <h3><a href="https://dispache.netlify.com/" >Todolist </a></h3>
-                      <span>Designed with REact,Firebase and css</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4 animate-box" data-animate-effect="fadeInTop">
-                <div className="project" style={{ backgroundImage: 'url(images/Music_App.png)' }}>
-                  <div className="desc">
-                    <div className="con">
-                      <h3><a href="work.html">Music App</a></h3>
-                      <span>Music App designed with react and using deezer API</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4 animate-box" data-animate-effect="fadeInBottom">
-                <div className="project" style={{ backgroundImage: 'url(images/monster.jpg)' }}>
-                  <div className="desc">
-                    <div className="con">
-                      <h3><a href="https://github.com/eddieir/Hayola">Hayola framework</a></h3>
-                      <span>deep learning model that takes hand-drawn web mockups and converts them into working HTML code</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4 animate-box" data-animate-effect="fadeInLeft">
-                <div className="project" style={{ backgroundImage: 'url(images/recommendation_system.jpg)' }}>
-                  <div className="desc">
-                    <div className="con">
-                      <h3><a href="https://github.com/eddieir/Recommendation_systems">recommendation system</a></h3>
-                      <span>Movie and Book recommendation systems</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4 animate-box" data-animate-effect="fadeInRight">
-                <div className="project" style={{ backgroundImage: 'url(images/1*q-h6a_z8IAiBwKZDhR0FhA.png)' }}>
-                  <div className="desc">
-                    <div className="con">
-                      <h3><a href="https://github.com/eddieir/live_twitter_sentiment_analysis">twitter sentiment analysis</a></h3>
-                      <span>twitter sentiment analysis with/without mood </span>
-                    </div>
-                  </div>
-                </div>
+            <div class="view view-tenth">
+              <img src="images/Herditama.jpg" width='300px' height='200px' />
+              <div class="mask">
+                <h2>Hotel reservation app</h2>
+                <p>Hotel reservation App <br />designed by reactjs</p>
+                <a href="https://herditama.netlify.com/" class="info">Read More</a>
               </div>
             </div>
-            <div className="row">
-              <div className="col-md-12 animate-box">
-                <p><a href="#" className="btn btn-primary btn-lg btn-load-more">Load more <i className="icon-reload" /></a></p>
+            <div class="view view-tenth">
+              <img src="images/dispache.png" width='300px' height='200px' />
+              <div class="mask">
+                <h2>Todolist app</h2>
+                <p>Todolist App <br />designed by reactjs<br />Used react hooks </p>
+                <a href="https://dispache.netlify.com/" class="info">Read More</a>
+              </div>
+            </div>
+            <div class="view view-tenth">
+              <img src="images/Music_App.png" width='300px' height='200px' />
+              <div class="mask">
+                <h2>Music player app</h2>
+                <p>Music player App <br />designed by reactjs<br />Used deezer api </p>
+                <a href="https://eddieir.github.io/Music_App_React/" class="info">Read More</a>
+              </div>
+            </div>
+
+            <div class="view view-tenth">
+              <img src="images/monster.jpg" width='300px' height='200px' />
+              <div class="mask">
+                <h2>Hayola framework</h2>
+                <p>Deep learning model<br />Takes hand-written web mockups<br />provides HTML code </p>
+                <a href="https://github.com/eddieir/Hayola" class="info">Read More</a>
+              </div>
+            </div>
+
+            <div class="view view-tenth">
+              <img src="images/recommendation_system.jpg" width='300px' height='200px' />
+              <div class="mask">
+                <h2>Recommendation systems</h2>
+                <p>Movie and books recommendation systems</p>
+                <a href="https://github.com/eddieir/Recommendation_systems" class="info">Read More</a>
+              </div>
+            </div>
+
+            <div class="view view-tenth">
+              <img src="images/1*q-h6a_z8IAiBwKZDhR0FhA.png" width='300px' height='200px' />
+              <div class="mask">
+                <h2>Twitter sentiment anslysis</h2>
+                <p>twitter sentiment analysis with/without mood</p>
+                <a href="https://github.com/eddieir/live_twitter_sentiment_analysis" class="info">Read More</a>
+              </div>
+            </div>
+
+            <div class="view view-tenth">
+              <img src="images/image-01-2019-ncov.jpg" width='300px' height='200px' />
+              <div class="mask">
+                <h2>CoronaVirus anslysis</h2>
+                <p>Coronavirus analysis and visulization </p>
+                <a href="https://github.com/eddieir/Corona_Analysis" class="info">Read More</a>
+              </div>
+            </div>
+
+            <div class="view view-tenth">
+              <img src="images/Analyst-believes-Bitcoin-price-is-priming-for-buy-zone-750x375.jpg" width='300px' height='200px' />
+              <div class="mask">
+                <h2>Bitcoin prediction</h2>
+                <p>Bitcoin prediction using LSTM neural net </p>
+                <a href="https://github.com/eddieir/bitcoin_prediction" class="info">Read More</a>
               </div>
             </div>
           </div>
@@ -85,3 +87,4 @@ export default class Projects extends Component {
     )
   }
 }
+
