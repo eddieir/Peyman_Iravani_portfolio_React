ffb043d4e1f25ee1af165b5f8171858dbcf3455e
peyman iravani <peyman.iravani@gmail.com>
2026-09-13 14:36:09 +0200

    Add alt attributes to image tags in projects component
    Updated image tags to include alt attributes for accessibility.

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -13,7 +13,7 @@ export default class Projects extends Component {
               </div>
             </div>
             <div class="view view-tenth">
-              <img src="images/Herditama.jpg" width='300px' height='200px' />
+              <img src="images/Herditama.jpg" width='300px' height='200px' alt="" />
               <div class="mask">
                 <h2>Hotel reservation app</h2>
                 <p>Hotel reservation App <br />designed by reactjs</p>
@@ -21,7 +21,7 @@ export default class Projects extends Component {
               </div>
             </div>
             <div class="view view-tenth">
-              <img src="images/dispache.png" width='300px' height='200px' />
+              <img src="images/dispache.png" width='300px' height='200px' alt="" />
               <div class="mask">
                 <h2>Todolist app</h2>
                 <p>Todolist App <br />designed by reactjs<br />Used react hooks </p>
@@ -29,7 +29,7 @@ export default class Projects extends Component {
               </div>
             </div>
             <div class="view view-tenth">
-              <img src="images/Music_App.png" width='300px' height='200px' />
+              <img src="images/Music_App.png" width='300px' height='200px' alt="" />
               <div class="mask">
                 <h2>Music player app</h2>
                 <p>Music player App <br />designed by reactjs<br />Used deezer api </p>
@@ -38,7 +38,7 @@ export default class Projects extends Component {
             </div>
 
             <div class="view view-tenth">
-              <img src="images/monster.jpg" width='300px' height='200px' />
+              <img src="images/monster.jpg" width='300px' height='200px' alt="" />
               <div class="mask">
                 <h2>Hayola framework</h2>
                 <p>Deep learning model<br />Takes hand-written web mockups<br />provides HTML code </p>
@@ -47,7 +47,7 @@ export default class Projects extends Component {
             </div>
 
             <div class="view view-tenth">
-              <img src="images/recommendation_system.jpg" width='300px' height='200px' />
+              <img src="images/recommendation_system.jpg" width='300px' height='200px' alt="" />
               <div class="mask">
                 <h2>Recommendation systems</h2>
                 <p>Movie and books recommendation systems</p>
@@ -56,7 +56,7 @@ export default class Projects extends Component {
             </div>
 
             <div class="view view-tenth">
-              <img src="images/1*q-h6a_z8IAiBwKZDhR0FhA.png" width='300px' height='200px' />
+              <img src="images/1*q-h6a_z8IAiBwKZDhR0FhA.png" width='300px' height='200px' alt="" />
               <div class="mask">
                 <h2>Twitter sentiment anslysis</h2>
                 <p>twitter sentiment analysis with/without mood</p>
@@ -65,7 +65,7 @@ export default class Projects extends Component {
             </div>
 
             <div class="view view-tenth">
-              <img src="images/image-01-2019-ncov.jpg" width='300px' height='200px' />
+              <img src="images/image-01-2019-ncov.jpg" width='300px' height='200px' alt="" />
               <div class="mask">
                 <h2>CoronaVirus anslysis</h2>
                 <p>Coronavirus analysis and visulization </p>
@@ -74,7 +74,7 @@ export default class Projects extends Component {
             </div>
 
             <div class="view view-tenth">
-              <img src="images/Analyst-believes-Bitcoin-price-is-priming-for-buy-zone-750x375.jpg" width='300px' height='200px' />
+              <img src="images/Analyst-believes-Bitcoin-price-is-priming-for-buy-zone-750x375.jpg" width='300px' height='200px' alt="" />
               <div class="mask">
                 <h2>Bitcoin prediction</h2>
                 <p>Bitcoin prediction using LSTM neural net </p>
